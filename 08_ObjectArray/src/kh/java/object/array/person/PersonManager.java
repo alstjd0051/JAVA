a37097c0a7ea2aa579a2566e1952bafef4a1079a
song min-seong <70365399+alstjd0051@users.noreply.github.com>
2021-01-06 23:40:46 +0900

    Add files via upload

---
+++ 08_ObjectArray/src/kh/java/object/array/person/PersonManager.java
@@ -0,0 +1,43 @@
+package kh.java.object.array.person;
+
+import java.util.Scanner;
+
+/**
+ * 연관관계
+ * PersonManager는 PErson을 field로써 참조한다.
+ * PersonManager ─────────> Person
+ *
+ */
+public class PersonManager {
+
+	private Person[] arr = new Person[3];
+	private Scanner sc = new Scanner(System.in);
+	
+	/**
+	 * Person정보 입력 메소드
+	 */
+	public void inputPerson() {
+		
+		for(int i = 0; i< arr.length; i++) {
+			System.out.println(" ------" + (i + 1) + "-------");
+			System.out.println("이름 : ");
+			String name = sc.next();
+			System.out.println("나이 : ");
+			int age = sc.nextInt();
+			Person p = new Person(name,age);
+			arr[i] = p;
+		};
+		
+		System.out.println("========================");
+	}
+	
+	/**
+	 * Person정보 출력 메소드
+	 */
+	public void printPerson() {
+		for(Person p : arr) {
+			p.printPerson();
+		}
+	}
+	
+}
